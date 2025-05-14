c9dc88c5c1d1a7bf24a85b3a57260e012187a57a
Chungharon <ngairaharon@gmail.com>
2025-05-14 20:11:02 +0300

    site updates

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,11 +5,10 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I&#39;m Haron.</h1>
+        <h1 className='title no-underline'>Hey, I&#39;m Haron Currenty working on this site😒🤣.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m a Software & DevOps Engineer based in Nairobi, Kenya. I&#39;m
-          passionate about learning new technologies and sharing knowledge with
-          others.
+          I&#39;m a Fullstack Dev based in Kiambu, Kenya. I&#39;m
+          passionate about automatomation deployment, monitoring, and security for cloud-based applications, integrating CI/CD pipelines.
         </p>
       </div>
       <div className='relative'>
