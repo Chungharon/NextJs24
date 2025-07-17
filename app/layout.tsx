b3cf0a0975b76f7b6a03ed56614ee0d6b963e691
Chungharon <ngairaharon@gmail.com>
2025-07-17 13:36:19 +0300

    DM_SANS font update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter, Playfair_Display } from 'next/font/google'
+import { DM_Sans } from 'next/font/google'
 
 import { cn } from '@/lib/utils'
 
@@ -8,11 +8,10 @@ import Providers from '@/components/providers'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  variable: '--font-serif'
+const dmSans = DM_Sans({ 
+  subsets: ['latin'], 
 })
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -29,8 +28,7 @@ export default function RootLayout({
       <body
         className={cn(
           'flex min-h-screen flex-col font-sans antialiased',
-          inter.variable,
-          playfair.variable
+          dmSans.className,
         )}
       >
         <Providers>
